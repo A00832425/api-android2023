97663febf92b98521d8f0bebf7e8a07206931124
A00832425 <113365949+A00832425@users.noreply.github.com>
2023-09-23 16:33:58 -0600

    Update routeuser.js

--- routes/routeuser.js
+++ routes/routeuser.js
@@ -5,6 +5,8 @@ const User = require("../model/user");
 const verifyToken = require("../middleware/verify");
 const bcrypt = require("bcrypt");
 
+const stripe = require('stripe')(sk_test_51NtOJqA7EOLFqNcIgdh0F9LzzS80PeItdHdQp5LEaEwXDQk3z4TLHt8Tax5dizhBDofKZ12b5ovG22vbpJ521XYJ00Im70Ns3X);
+
 // Registration Endpoint
 app.post("/register", async (req, res) => {
   try {
@@ -58,6 +60,41 @@ app.post("/login", async (req, res) => {
     res.status(500).json({ message: "Error" });
   }
 });
+
+
+app.post('/process-payment', async (req, res) => {
+  try {
+    const { cardNumber, expiration, cvc } = req.body;
+
+    // Create a payment intent using the Stripe API
+    const paymentIntent = await stripe.paymentIntents.create({
+      amount: 1000, // Change to your desired amount in cents (e.g., $10)
+      currency: 'usd', // Change to your desired currency
+      payment_method_types: ['card'],
+      payment_method_data: {
+        card: {
+          number: cardNumber,
+          exp_month: expiration.split('/')[0],
+          exp_year: expiration.split('/')[1],
+          cvc,
+        },
+      },
+    });
+
+    // Confirm the payment intent
+    await stripe.paymentIntents.confirm(paymentIntent.id);
+
+    // Payment successful
+    res.status(200).json({ success: true });
+  } catch (error) {
+    // Payment failed
+    console.error(error);
+    res.status(500).json({ success: false, error: error.message });
+  }
+});
+
+
+
 
 app.get("/protected", verifyToken, (req, res) => {
   res.status(200).json({ message: "Acceso permitido" });
