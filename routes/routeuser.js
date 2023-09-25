ea30c9ab12149c11ada5bf5d204a6d713b0da99a
A00832425 <113365949+A00832425@users.noreply.github.com>
2023-09-25 01:18:57 -0600

    Update routeuser.js

--- routes/routeuser.js
+++ routes/routeuser.js
@@ -78,9 +78,9 @@ app.post('/process-payment', async (req, res) => {
           exp_month: expiration.split('/')[0],
           exp_year: expiration.split('/')[1],
           cvc,
-          type: 'card',
         },
       },
+      type: 'card',
     });
 
     // Confirm the payment intent
