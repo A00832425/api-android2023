46d29dfcd1e1887c0dd5db336895e0c18a7e5da7
A00832425 <113365949+A00832425@users.noreply.github.com>
2023-10-14 19:41:20 -0600

    Update routeorg.js

--- routes/routeorg.js
+++ routes/routeorg.js
@@ -7,12 +7,14 @@ const verifyToken = require("../middleware/verify");
 
 app.post("/add", async (req, res) => {
   try {
+    console.log(req.body)
     const { email, name, description, img, linkb1, linkb2, linkb4} = req.body;
-
+    console.log(linkb1)
     const newOrg = new Org({ email, name, description });
     await newOrg.save().then(savedObject => {
     const newPag = new Pag({ titulo: name, desc: description, img, linkb1, linkb2, linkb4, orgId: savedObject._id.toString()});
     console.log('Object saved with ID:', savedObject._id.toString());
+    console.log(newPag)
     // You can access the ObjectId via savedObject._id
   }
       );
