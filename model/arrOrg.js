e265919a90a8c9e7f016cddaad58483ce1d26db8
A00832425 <113365949+A00832425@users.noreply.github.com>
2023-10-16 18:22:56 -0600

    Update arrOrg.js

--- model/arrOrg.js
+++ model/arrOrg.js
@@ -1,19 +1,10 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
-
-// Define the schema for the "Orgs" model
-const orgSchema = Schema({
-  // Define the fields and their types for your "Orgs" model
-  name: String,
-  // other fields...
-});
-
-// Create the "Orgs" model
-const Org = mongoose.model("Orgs", orgSchema);
+const Org = require("../model/org");
 
 // Define the schema for the "arrOrgs" model
 const arrOrgSchema = Schema({
-  arr: [{ type: Schema.Types.ObjectId, ref: 'Orgs' }],
+  arr: [{ type: Schema.Types.ObjectId, ref: 'Org' }],
 });
 
 module.exports = mongoose.model("arrOrgs", arrOrgSchema);
