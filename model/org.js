59556f0fba6a0b4efd926beb6c93de5f0771adb0
A00832425 <113365949+A00832425@users.noreply.github.com>
2023-10-16 19:05:27 -0600

    Update org.js

--- model/org.js
+++ model/org.js
@@ -5,6 +5,7 @@ const OrgSchema = Schema({
   email: { type: String, required: true },
   name: { type: String, required: true },
   description: { type: String, default: "" },
+  orgTags: [{type: String}]
 });
 
 module.exports = mongoose.model("orgs", OrgSchema);
